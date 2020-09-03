29d90d51e6535a1460d9ac5296212a296e220046
kamla-baraka <44601580+kamla-baraka@users.noreply.github.com>
2020-09-03 09:54:26 -0700

    Updates

---
+++ precache-manifest.7aa503aa2253813f24d7176fc3baefea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13adb8d2d0aa4219a1b58a4754e8f7d6",
+    "url": "/projectReact/index.html"
+  },
+  {
+    "revision": "c1662b2d26dfe33aece4",
+    "url": "/projectReact/static/css/main.28a67209.chunk.css"
+  },
+  {
+    "revision": "0d9085c05c10fd138f9f",
+    "url": "/projectReact/static/js/2.f5fe1b40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projectReact/static/js/2.f5fe1b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1662b2d26dfe33aece4",
+    "url": "/projectReact/static/js/main.9e887c9e.chunk.js"
+  },
+  {
+    "revision": "f6a4bb3ab8ec6ce42640",
+    "url": "/projectReact/static/js/runtime-main.fda5669c.js"
+  }
+]);
